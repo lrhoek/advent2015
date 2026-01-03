175264085b995e05dc854046c6d5178f5e1dd144
Laurens Hoek <lrhoek@gmail.com>
2026-01-03 20:41:18 +0100

    Day 1

---
+++ 01/01.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Anarchitecture\pipe as p;
+
+$input = file_get_contents('input');
+
+$floor = substr_count($input, "(") - substr_count($input, ")");
+
+$position = $input
+    |> str_split(...)
+    |> p\array_reduce_until(
+        fn ($current, $instruction) => $current + ($instruction === "(" ? 1 : -1),
+        fn ($current) => $current === -1
+    )
+    |> p\array_nth(1)
+    |> p\increment();
+
+echo $floor . PHP_EOL;
+echo $position . PHP_EOL;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2015
+
+[Advent of Code 2015](https://adventofcode.com/2015) done in PHP with the [Anarchitecture/pipe](https://github.com/Anarchitecture/pipe) library.
