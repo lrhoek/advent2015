7dd8b0f89896480ca74fc8fb3e5ff819bd917d5c
Laurens Hoek <lrhoek@gmail.com>
2026-01-04 11:23:22 +0100

    Day 4 lib update

--- 04/04.php
+++ 04/04.php
@@ -6,14 +6,11 @@ use Anarchitecture\pipe as p;
 
 $key = file_get_contents('input');
 
-function find(string $key, string $needle) : string {
+function find(string $key, string $needle) : int {
     return p\iterable_ticker()
-            |> p\iterable_filter(fn($number) => str_starts_with(md5($key . $number), $needle))
-            |> p\iterable_current();
+        |> p\iterable_filter(fn($number) => $key.$number |> md5(...) |> p\str_starts_with($needle))
+        |> p\iterable_first(...);
 }
 
-$number = find($key, "00000");
-$number2 = find($key, "000000");
-
-echo $number . PHP_EOL;
-echo $number2 . PHP_EOL;
+echo find($key, "00000") . PHP_EOL;
+echo find($key, "000000") . PHP_EOL;
