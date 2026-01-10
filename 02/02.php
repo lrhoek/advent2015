1772b20fe86d26ee00079aa1e2c8e0079eb00083
Laurens Hoek <lrhoek@gmail.com>
2026-01-10 13:24:23 +0100

    Day 02 tweaks

--- 02/02.php
+++ 02/02.php
@@ -1,21 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 require_once '../vendor/autoload.php';
 
 use Anarchitecture\pipe as p;
 
+function wrapping_paper(int $d1, int $d2, int $d3) : int {
+    return 3 * $d1 * $d2 + 2 * $d2 * $d3 + 2 * $d3 * $d1;
+}
+
+function ribbon(int $d1, int $d2, int $d3) : int {
+    return 2 * $d1 + 2 * $d2 + $d1 * $d2 * $d3;
+}
+
+function total(array $input, callable $mapper) : int {
+    return $input
+        |> p\array_map(p\sort())
+        |> p\array_map(p\apply($mapper))
+        |> array_sum(...);
+}
+
+function dimensions(string $input) : array {
+    return $input
+        |> p\explode('x')
+        |> p\array_map(intval(...))
+        |> array_values(...);
+}
+
 $input = file_get_contents('input')
     |> p\explode(PHP_EOL)
-    |> p\array_map(p\explode('x'))
-    |> p\array_map(p\sort());
+    |> p\array_map(dimensions(...))
+    |> array_values(...);
 
-$paper = $input
-    |> p\array_map(p\apply(fn ($d1, $d2, $d3) => 3 * $d1 * $d2 + 2 * $d2 * $d3 + 2 * $d3 * $d1))
-    |> array_sum(...);
-
-$ribbon = $input
-    |> p\array_map(p\apply(fn ($d1, $d2, $d3) => 2 * $d1 + 2 * $d2 + $d1 * $d2 * $d3))
-    |> array_sum(...);
-
-echo $paper . PHP_EOL;
-echo $ribbon . PHP_EOL;
+echo total($input, wrapping_paper(...)) . PHP_EOL;
+echo total($input, ribbon(...)) . PHP_EOL;
