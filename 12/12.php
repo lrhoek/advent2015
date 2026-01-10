27ff4f799b20c17d2b5c93b5e7c0184a6fc0f78d
Laurens Hoek <lrhoek@gmail.com>
2026-01-10 01:38:38 +0100

    Day 12

--- 12/12.php
+++ 12/12.php
@@ -6,37 +6,26 @@ require_once '../vendor/autoload.php';
 
 use Anarchitecture\pipe as p;
 
-function include_all(array|object $data) : array {
-    return (array) $data;
+function include_all(object $data) : array {
+    return get_object_vars($data);
 }
 
-function exclude_red(array|object $data) : array {
-    return match (is_object($data)) {
-        true => in_array("red", (array) $data, true) ? [] : (array) $data,
-        default => $data
-    };
+function exclude_red(object $data) : array {
+    return $data
+        |> get_object_vars(...)
+        |> p\when(fn ($x) => in_array("red", $x, true), fn ($_) => [])
+        |> array_values(...);
 }
 
-function sum(mixed $data, callable $policy) : int {
-    return match (true) {
-        is_int($data) || is_float($data) => (int) $data,
-        is_array($data) || is_object($data) => $data
-            |> $policy
-            |> array_values(...)
-            |> p\array_map(fn ($item) => sum($item, $policy))
-            |> array_sum(...),
-        default => 0
-    };
+function sum($data, callable $normalizer) : int {
+    return $data
+        |> p\when(is_object(...), $normalizer)
+        |> p\when(is_array(...), p\array_reduce(fn ($sum, $x) => $sum + sum($x, $normalizer), 0))
+        |> intval(...);
 }
 
 $input = file_get_contents('input')
     |> json_decode(...);
 
-$policies = [
-    include_all(...),
-    exclude_red(...)
-];
-
-foreach ($policies as $policy) {
-    echo sum($input, $policy) . PHP_EOL;
-}
+echo sum($input, include_all(...)) . PHP_EOL;
+echo sum($input, exclude_red(...)) . PHP_EOL;
