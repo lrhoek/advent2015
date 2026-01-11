35f0c15be52597addccdd075fbb6986953e7174a
Laurens Hoek <lrhoek@gmail.com>
2026-01-11 13:36:15 +0100

    Day 14

---
+++ 14/14.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '../vendor/autoload.php';
+
+use Anarchitecture\pipe as p;
+
+
+function reindeer(string $reindeer) : array {
+    return $reindeer
+        |> p\preg_match("/(\d+) .* (\d+) .* (\d+)/")
+        |> p\array_slice(1)
+        |> array_values(...);
+}
+
+function fly($reindeer, $seconds) : int {
+
+    [$speed, $flying_limit, $resting] = $reindeer;
+    $distance = 0;
+
+    while ($seconds > 0) {
+        $distance += min($seconds, $flying_limit) * $speed;
+        $seconds -= $flying_limit + $resting;
+    }
+
+    return $distance;
+}
+
+function distances($reindeer, $seconds) : array {
+    return $reindeer
+        |> p\array_map(fn ($deer) => fly($deer, $seconds))
+        |> array_values(...);
+}
+
+function points($reindeer, $seconds) : array {
+
+    $points = $reindeer
+        |> p\array_map(p\value(0))
+        |> array_values(...);
+
+    while ($seconds > 0) {
+        $distances = distances($reindeer, $seconds);
+
+        $leaders = $distances
+            |> p\array_filter(p\equals(max($distances)))
+            |> array_keys(...);
+
+        foreach ($leaders as $leader) {
+            $points[$leader]++;
+        }
+
+        $seconds--;
+    }
+
+    return $points;
+}
+
+function winner(array $reindeer, callable $system, int $seconds) : int {
+    return $system($reindeer, $seconds)
+        |> max(...);
+}
+
+$reindeer = file_get_contents('input')
+    |> p\explode(PHP_EOL)
+    |> p\array_map(reindeer(...))
+    |> array_values(...);
+
+echo winner($reindeer, distances(...), 2503) . PHP_EOL;
+echo winner($reindeer, points(...), 2503) . PHP_EOL;
