b0c24b872cf899fd73c27a42cf6862c8a889ba35
Laurens Hoek <lrhoek@gmail.com>
2026-01-13 22:10:07 +0100

    Day 15 with dissoc and collect

--- 15/15.php
+++ 15/15.php
@@ -1,38 +1,42 @@
 <?php
 
-
 declare(strict_types=1);
 
 require_once '../vendor/autoload.php';
 
 use Anarchitecture\pipe as p;
 
-function ingredient(string $ingredient) {
+function ingredient(string $ingredient) : array {
     return $ingredient
         |> p\preg_match_all("/([a-z]+) (-?\d+)/", PREG_SET_ORDER)
         |> p\array_reduce(fn ($properties, $property) => [$property[1] => $property[2]] + $properties, [])
-        |> p\array_map(intval(...));
+        |> p\array_map(intval(...))
+        |> p\collect(...);
 }
 
-function score(array $recipe, array $ingredients, ?int $calorie_target) : int {
+function cookie(array $recipe, array $ingredients) : array {
     return $recipe
         |> p\iterable_zip($ingredients)
         |> p\iterable_map(p\apply(fn ($amount, $ingredient) => $ingredient |> p\array_map(fn ($property) => $amount * $property)))
-        |> iterator_to_array(...)
+        |> p\collect(...)
         |> p\array_transpose()
         |> p\array_map(fn ($property) => max(0, array_sum($property)))
-        |> p\if_else(
-            fn ($properties) => is_int($calorie_target) && $properties["calories"] !== $calorie_target,
-            fn ($properties) => (["calories" => 0] + $properties),
-            fn ($properties) => (["calories" => 1] + $properties)
-        )
-        |> array_product(...);
+        |> p\collect(...);
 }
 
-function best(array $ingredients, ?int $calorie_target = null) : int {
+function score(array $cookie) : int {
+    return $cookie
+        |> p\array_dissoc("calories")
+        |> array_product(...)
+        |> intval(...);
+}
+
+function best(array $ingredients, int $teaspoons, ?int $calorie_target = null) : int {
     return $ingredients
-        |> p\iterable_allocate(100)
-        |> p\iterable_map(fn($recipe) => score($recipe, $ingredients, $calorie_target))
+        |> p\iterable_allocate($teaspoons)
+        |> p\iterable_map(fn($recipe) => cookie($recipe, $ingredients))
+        |> p\iterable_filter(fn ($properties) => !is_int($calorie_target) || $properties["calories"] === $calorie_target)
+        |> p\iterable_map(score(...))
         |> p\iterable_reduce(fn ($max, $score) => max($max, $score), 0)
         |> intval(...);
 }
@@ -42,5 +46,5 @@ $ingredients = file_get_contents('input')
     |> p\explode(PHP_EOL)
     |> p\array_map(ingredient(...));
 
-echo best($ingredients) . PHP_EOL;
-echo best($ingredients, 500) . PHP_EOL;
+echo best($ingredients, 100) . PHP_EOL;
+echo best($ingredients, 100, 500) . PHP_EOL;
