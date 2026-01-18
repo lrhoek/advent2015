a64a769915700f4fd533cbfba699db5cf505bd29
Laurens Hoek <lrhoek@gmail.com>
2026-01-18 20:12:17 +0100

    Day 20

--- 20/20.php
+++ 20/20.php
@@ -5,14 +5,25 @@ declare(strict_types=1);
 require_once '../vendor/autoload.php';
 
 use MathPHP\Algebra as a;
+use Anarchitecture\pipe as p;
 
-$input = file_get_contents('input');
-$sum = 0;
+function house (int $target, int $presents_per_house, ?int $presents_per_elf = null) : int {
 
-for ($i = 1; $sum < $input / 10; $i++) {
-    $sum = $i
-        |> a::factors(...)
-        |> array_sum(...);
+    for ($house = 1, $presents = 0; $presents < $target / $presents_per_house; $house++) {
+        $presents = $house
+            |> a::factors(...)
+            |> p\when(
+                p\value($presents_per_elf !== null),
+                p\array_filter(fn ($elf) => $house <= $elf * $presents_per_elf)
+            )
+            |> array_sum(...);
+    }
+
+    return $house - 1;
 }
 
-echo $i - 1 . PHP_EOL;
+$target = file_get_contents('input')
+    |> intval(...);
+
+echo house($target, 10) . PHP_EOL;
+echo house($target, 11, 50) . PHP_EOL;
