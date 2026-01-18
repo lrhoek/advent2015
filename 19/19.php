5a0d07bb75697aefdcbe4c96fffbdd3365b09ae6
Laurens Hoek <lrhoek@gmail.com>
2026-01-18 14:41:56 +0100

    Day 19

--- 19/19.php
+++ 19/19.php
@@ -1,8 +1,53 @@
 <?php
 
+declare(strict_types=1);
+
 require_once '../vendor/autoload.php';
 
 use Anarchitecture\pipe as p;
+
+function steps_to(mixed $molecules, array $replacements, string $target, int $steps = 1) : int {
+    return $molecules
+        |> p\when(p\not(is_array(...)), fn ($molecule) => [$molecule])
+        |> p\array_map(fn($molecule) => replaced($replacements, $molecule))
+        |> p\array_flatten(...)
+        |> p\array_unique()
+        |> p\usort(fn($a, $b) => strlen($a) <=> strlen($b))
+        |> p\array_slice(0, 1)
+        |> p\collect(...)
+        |> p\if_else(
+            fn ($molecules) => in_array($target, $molecules),
+            p\value($steps),
+            fn ($molecules) => steps_to($molecules, $replacements, $target, $steps + 1)
+        )
+        |> intval(...);
+}
+
+function replaced(array $replacements, string $molecule) : array {
+    return $replacements
+        |> p\array_map(p\apply(fn($token, $replacement) => replace($molecule, $token, $replacement) |> p\collect(...)))
+        |> p\array_flatten(...)
+        |> p\array_unique()
+        |> p\collect(...);
+}
+
+function replace(string $molecule, string $token, string $replacement) : Generator {
+
+    $position = strpos($molecule, $token);
+
+    if ($position === false) {
+        return;
+    }
+
+    $head = substr($molecule, 0, $position);
+    $tail = substr($molecule, $position + strlen($token));
+
+    yield $head . $replacement . $tail;
+
+    foreach (replace($tail, $token, $replacement) as $rest) {
+        yield $head . $token . $rest;
+    }
+}
 
 [$replacements, $molecule] = file_get_contents('input')
     |> trim(...)
@@ -16,37 +61,9 @@ $replacements = $replacements
 
 $distinct = replaced($replacements, $molecule);
 
+$replacements = $replacements
+    |> p\array_map(array_reverse(...))
+    |> p\collect(...);
+
 echo count($distinct) . PHP_EOL;
-
-// @TODO Iterate over this for part 2: molecule[] => molecule[][] => flatten
-function replaced(array $replacements, string $molecule) : array {
-    return $replacements
-        |> p\array_map(p\apply(fn($token, $replacement) => replace($molecule, $token, $replacement) |> p\collect(...)))
-        |> p\array_flatten(...)
-        |> p\array_unique()
-        |> p\collect(...);
-}
-
-function replace($molecule, $token, $replacement) : Generator {
-
-    $molecule = explode($token, $molecule);
-
-    if (count($molecule) === 1) {
-        return;
-    }
-
-    $head = array_shift($molecule);
-
-    $replaced = $molecule;
-
-    $third = array_shift($replaced);
-
-    array_unshift($replaced, $head . $replacement . $third);
-
-    yield join($token, $replaced);
-
-    foreach (replace(join($token, $molecule), $token, $replacement) as $rest) {
-        yield $head . $token . $rest;
-    }
-
-}
+echo steps_to($molecule, $replacements, "e") . PHP_EOL;
